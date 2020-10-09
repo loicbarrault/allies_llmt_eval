bf32125f4763c8209a09a9974a041b84d7dd581a
loicbarrault <loic.barrault@gmail.com>
2020-10-09 11:52:55 +0100

    added sacrebleu scoring

--- eval.bash
+++ eval.bash
@@ -5,6 +5,7 @@ detok='/home/barrault/git/mosesdecoder/scripts/tokenizer/detokenizer.perl -q '
 mbleu='/home/barrault/git/mosesdecoder/scripts/generic/multi-bleu.perl '
 data_dir='/home/barrault/projects/allies/allies_llmt_data/en-fr/en-fr/fr'
 ref_dir='reference'
+file_order='/home/barrault/projects/allies/allies_llmt_eval/allies.nt14.file_order'
 
 if [[ $# -lt 1 ]]; then
   echo "Usage: $0 <experiment directory name> "
@@ -17,6 +18,7 @@ if ! [[ -d original ]] || ! [[ -d adapted ]]; then
 fi
 
 # prepare reference
+rm -fR $ref_dir
 mkdir -p $ref_dir
 
 # If a file was not using interactive or active learning, simply copy the original file
@@ -30,6 +32,8 @@ do
 
 done
 
+# Prepare multi-bleu.perl reference file
+cat $ref_dir/* > $ref_dir.all.txt
 
 # detokenize
 for dir in original adapted
@@ -52,14 +56,27 @@ do
     echo $f
     $mbleu $ref_dir/$f.txt < $dir.detok/$f >> results.$dir
   done
+
+  # Create merged file and score it
+  cat $dir.detok/* > $dir.all.txt
+  $mbleu $ref_dir.all.txt < $dir.all.txt > results.$dir.all.mbleu
 done
 
-# Create merged original and adapted files
-cat original.detok/* > original.all.txt
-cat adapted.detok/* > adapted.all.txt
-
-$mbleu $ref_dir/$f.txt < $dir.detok/$f >> results.$dir
+# sacrebleu scoring
+for dir in original adapted
+do
+  rm -f $dir.all.sacrebleu.txt
+# prepare file in sacrebleu order
+  for f in `cat $file_order`
+  do
+    cat $dir.detok/$f >> $dir.all.sacrebleu.txt
+  done
+# score it with sacrebleu
+  sacrebleu -t wmt14 -l en-fr -i $dir.all.sacrebleu.txt > results.$dir.all.sacrebleu
+done
 
 mkdir $1
-mv original* adapted* results* $1
+mv original* adapted* results* $ref_dir.all.txt $1
+
+
 
